a74c5c62130581591416e4cdcc306352499b4f4e
raphael <raphael.rodrigues@vilt-group.com>
2019-04-11 15:19:04 +0100

    Test

--- 1. basics/alerts.js
+++ 1. basics/alerts.js
@@ -8,7 +8,7 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch()
+  const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']})
   const page = await browser.newPage()
   await page.goto('https://www.google.com/')
   page.on('dialog', async dialog => {
